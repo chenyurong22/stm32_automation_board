3f0461da4e9e35be935345703f54c3415a049d29
vxnwt846 <benhassenmahdi@gmail.com>
2026-06-02 22:14:40 +0100

    fix: add missing stm32f4xx_hal_conf.h with all required HAL module enables

---
+++ inc/stm32f4xx_hal_conf.h
@@ -0,0 +1,47 @@
+#ifndef STM32F4XX_HAL_CONF_H
+#define STM32F4XX_HAL_CONF_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define HSE_VALUE    8000000U
+#define HSI_VALUE    16000000U
+#define LSE_VALUE    32768U
+#define LSI_VALUE    32000U
+
+#define  VDD_VALUE  3300U
+
+#define USE_HAL_ADC_REGISTER_CALLBACKS         0U
+#define USE_HAL_CAN_REGISTER_CALLBACKS         0U
+#define USE_HAL_DAC_REGISTER_CALLBACKS         0U
+#define USE_HAL_ETH_REGISTER_CALLBACKS         0U
+#define USE_HAL_I2C_REGISTER_CALLBACKS         0U
+#define USE_HAL_RTC_REGISTER_CALLBACKS         0U
+#define USE_HAL_SPI_REGISTER_CALLBACKS         0U
+#define USE_HAL_TIM_REGISTER_CALLBACKS         0U
+#define USE_HAL_UART_REGISTER_CALLBACKS        0U
+#define USE_HAL_USART_REGISTER_CALLBACKS       0U
+
+#define HAL_MODULE_ENABLED
+#define HAL_ADC_MODULE_ENABLED
+#define HAL_CORTEX_MODULE_ENABLED
+#define HAL_DAC_MODULE_ENABLED
+#define HAL_DMA_MODULE_ENABLED
+#define HAL_ETH_MODULE_ENABLED
+#define HAL_FLASH_MODULE_ENABLED
+#define HAL_GPIO_MODULE_ENABLED
+#define HAL_PWR_MODULE_ENABLED
+#define HAL_RCC_MODULE_ENABLED
+#define HAL_TIM_MODULE_ENABLED
+#define HAL_UART_MODULE_ENABLED
+
+#define ASSERT(...)     do { } while(0)
+
+#include "stm32f4xx_hal_def.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* STM32F4XX_HAL_CONF_H */
